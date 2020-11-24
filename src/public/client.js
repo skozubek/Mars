0d5fa283f3a07205df8bc69f7d7c461a0d227105
Sergi <sergiusz.kozubek@gmail.com>
2020-11-24 16:22:33 +0100

    Added list to choose rover. Created render functions dependant on chosen rover. Made store mutable.

--- src/public/client.js
+++ src/public/client.js
@@ -1,33 +1,94 @@
+// keeps the App state - mutable
 let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+  apod: '',
+  selectedRover: '',
+  // rovers list is immutable
+  rovers: Immutable.List(['Curiosity', 'Opportunity', 'Spirit']),
+};
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById('root');
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+const updateStore = (newState) => {
+  store = Object.assign(store, newState);
+  render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
+  console.log(`In render ${state.selectedRover}`);
+  root.innerHTML = App(state);
+};
+// Pure function that renders header
+const renderHeader = () => `
+        <h1>Hello! Welocome on Mars Rovers Dashboard!</h1>
+    `;
+// Pure function that renders list of rovers to choose from
+const renderRoversList = (rovers, selectedRover) => {
+  console.log(`in render ${selectedRover}`);
+  if (selectedRover === 'Curiosity') {
+    return `
+              <select name="rovers" id="rovers" onchange="selectRover()">
+                <option value="${rovers.get(0)}">${rovers.get(0)}</option>
+                <option value="${rovers.get(1)}">${rovers.get(1)}</option>
+                <option value="${rovers.get(2)}">${rovers.get(2)}</option>
+              </select>
+            `;
+  }
 
+  if (selectedRover === 'Opportunity') {
+    return `
+              <select name="rovers" id="rovers" onchange="selectRover()">
+                <option value="${rovers.get(1)}">${rovers.get(1)}</option>
+                <option value="${rovers.get(0)}">${rovers.get(0)}</option>
+                <option value="${rovers.get(2)}">${rovers.get(2)}</option>
+              </select>
+            `;
+  }
+  if (selectedRover === 'Spirit') {
+    return `
+              <select name="rovers" id="rovers" onchange="selectRover()">
+                <option value="${rovers.get(2)}">${rovers.get(2)}</option>
+                <option value="${rovers.get(0)}">${rovers.get(0)}</option>
+                <option value="${rovers.get(1)}">${rovers.get(1)}</option>
+              </select>
+            `;
+  }
+
+  return `
+              <select name="rovers" id="rovers" onchange="selectRover()">
+                <option value="">select rover</option>
+                <option value="${rovers.get(0)}">${rovers.get(0)}</option>
+                <option value="${rovers.get(1)}">${rovers.get(1)}</option>
+                <option value="${rovers.get(2)}">${rovers.get(2)}</option>
+              </select>
+            `;
+};
+
+// Pure function that renders selected rover heading
+const renderSelectedRoverHeader = (roverName) => {
+  if (roverName !== '') {
+    return `
+      <h3>Let's see what ${roverName}'s got for you:)</h3>
+        `;
+  }
+
+  return `
+        <h1>Hello!</h1>
+    `;
+};
 
 // create content
 const App = (state) => {
-    let { rovers, apod } = state
+  const { rovers, apod, selectedRover } = state;
+  console.log(`in App ${selectedRover}`);
 
-    return `
-        <header></header>
+  return `
+        <header>${renderHeader()}</header>
         <main>
-            ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
+                <h3>Choose the rover from the list below:</h3>
+                ${renderRoversList(rovers, selectedRover)}
+                ${renderSelectedRoverHeader(selectedRover)}
                 <p>
                     One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
                     the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
@@ -40,66 +101,55 @@ const App = (state) => {
             </section>
         </main>
         <footer></footer>
-    `
-}
+    `;
+};
 
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
-    render(root, store)
-})
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
-
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
-    return `
-        <h1>Hello!</h1>
-    `
-}
-
+const selectRover = () => {
+  const selectedRover = document.getElementById('rovers').value;
+  updateStore({ selectedRover });
+  console.log(`dupa ${store.selectedRover}`);
+};
 // Example of a pure function that renders infomation requested from the backend
 const ImageOfTheDay = (apod) => {
+  // If image does not already exist, or it is not from today -- request it again
+  const today = new Date();
+  const photodate = new Date(apod.date);
+  console.log(apod, photodate.getDate(), today.getDate());
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+  console.log(photodate.getDate() === today.getDate());
+  if (!apod || apod.date === today.getDate()) {
+    getImageOfTheDay(store);
+  }
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
-
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
+  // check if the photo of the day is actually type video!
+  if (apod.media_type === 'video') {
+    return `
             <p>See today's featured video <a href="${apod.url}">here</a></p>
             <p>${apod.title}</p>
             <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
+        `;
+  }
+  return `
             <img src="${apod.image.url}" height="350px" width="100%" />
             <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+        `;
+};
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
 const getImageOfTheDay = (state) => {
-    let { apod } = state
+  const { apod } = state;
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+  fetch(`http://localhost:3000/apod`)
+    .then((res) => res.json())
+    .then((apod) => updateStore({ apod }));
 
-    return data
-}
+  return data;
+};
--- src/server/index.js
+++ src/server/index.js
@@ -1,28 +1,37 @@
-require('dotenv').config()
-const express = require('express')
-const bodyParser = require('body-parser')
-const fetch = require('node-fetch')
-const path = require('path')
+/**
+ * Required External Modules
+ */
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const path = require('path');
 
-const app = express()
-const port = 3000
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || '4000';
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // your API calls
-
+/**
+ * Routes Definitions
+ */
 // example API call
 app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+  try {
+    const image = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+    ).then((res) => res.json());
+    res.send({ image });
+  } catch (err) {
+    console.log('error:', err);
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
