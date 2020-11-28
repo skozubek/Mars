f76a2d3218826cab6cbd0d22f94a01a66f48915e
Sergi <sergiusz.kozubek@gmail.com>
2020-11-28 16:41:44 +0100

    Removed starter  code for nasa's photo of the day

--- src/public/client.js
+++ src/public/client.js
@@ -102,7 +102,6 @@ const renderImages = (images) => {
     return renderImgUrls;
   }
 };
-
 // create content
 const App = () => {
   const { rovers, apod, selectedRover, latestPhotos } = store;
@@ -120,22 +119,14 @@ const App = () => {
   return `
         <header>${renderHeader(rovers, selectedRover)}</header>
         <main>
-            <section>
-                <p>
-                    Look!
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
         </main>
         <footer></footer>
     `;
 };
-
 // listening for load event because page should load before any JS is called
 window.addEventListener('load', () => {
   render(root, store);
 });
-
 // Handles changes in rovers select dropdown list
 const selectRover = () => {
   const selectedElement = document.getElementById('rovers');
@@ -144,40 +135,6 @@ const selectRover = () => {
     updateStore({ selectedRover });
     getRoverData(store);
   }
-};
-
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-  // If image does not already exist, or it is not from today -- request it again
-  const today = new Date();
-  const photodate = new Date(apod.date);
-  if (!apod || apod.date === today.getDate()) {
-    getImageOfTheDay(store);
-  }
-
-  // check if the photo of the day is actually type video!
-  if (apod.media_type === 'video') {
-    return `
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `;
-  }
-  return `
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation.substring(0, 100)}</p>
-        `;
-};
-
-// ------------------------------------------------------  API CALLS
-// API call to get select image of the day
-const getImageOfTheDay = (state) => {
-  const { apod } = state;
-
-  fetch(`http://localhost:3000/apod`)
-    .then((res) => res.json())
-    .then((apod) => updateStore({ apod }));
-  return apod;
 };
 // API call to get select rover's data
 const getRoverData = (state) => {
