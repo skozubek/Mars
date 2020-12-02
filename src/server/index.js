34ee7f7610a5785b71ba7f1e51f5be110c1f0788
Sergi <sergiusz.kozubek@gmail.com>
2020-12-02 13:27:24 +0100

    API key for Heroku

--- src/server/index.js
+++ src/server/index.js
@@ -37,8 +37,7 @@ app.get('/apod', async (req, res) => {
 app.get('/rovers', async (req, res) => {
   try {
     const { name } = req.query;
-    console.log(process.env.API_KEY);
-    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=${process.env.API_KEY}`;
+    const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${name}/latest_photos?api_key=AZ6takwLFX1CPatIIRa55vYmZI5idA13pcPuqoLE`;
     const photos = await fetch(url).then((res) => res.json());
     res.send({ photos });
   } catch (err) {
